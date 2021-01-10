ebccd593711152cdecc3eab6be0383feddbb3c4c
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2021-01-11 00:27:22 +0900

    Friend 엔티티 추가

---
+++ src/main/java/com/stay/stay/domain/Friend.java
@@ -0,0 +1,40 @@
+package com.stay.stay.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "FRIEND")
+@Getter @Setter
+public class Friend {
+
+    @Id @GeneratedValue
+    @Column(name = "friend_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String name;
+    private String profile_image;
+    private int current_record;
+    private int best_record;
+    private boolean isPrivate;
+
+
+    @Builder
+    private Friend(User user, String name, String profile_image, int current_record, int best_record, boolean isPrivate) {
+
+        this.user = user;
+        user.getFriendSet().add(this);
+        this.name = name;
+        this.profile_image = profile_image;
+        this.current_record = current_record;
+        this.best_record = best_record;
+        this.isPrivate = isPrivate;
+    }
+}
