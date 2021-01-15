53d20263cc98b217a70250943684338f9d1549ed
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2021-01-15 23:52:49 +0900

    Api Constant 생성

---
+++ src/main/java/com/stay/stay/constants/Message.java
@@ -0,0 +1,30 @@
+package com.stay.stay.constants;
+
+import lombok.Data;
+
+@Data
+public class Message {
+
+    private int status;
+    private String message;
+    private Object data;
+
+    public Message() {
+        this.status = StatusCode.BAD_REQUEST;
+        this.data = null;
+        this.message = null;
+    }
+
+    public Message(int status, String message) {
+        this.status = status;
+        this.message = message;
+        this.data = null;
+    }
+
+    public Message(int status, String message, Object data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+}
+
---
+++ src/main/java/com/stay/stay/constants/ResponseMessage.java
@@ -0,0 +1,9 @@
+package com.stay.stay.constants;
+
+public class ResponseMessage {
+
+    public static final String Update_Tos_Agree_Date = "약관 동의 날짜 변경 성공";
+
+    public static final String NotFound = "Not Found Exception";
+
+}
