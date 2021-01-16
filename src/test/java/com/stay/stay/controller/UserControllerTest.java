a1e6befeea06ecc61d14abdf8789c756d5711872
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2021-01-17 03:08:23 +0900

    테스트 코드 추가

---
+++ src/test/java/com/stay/stay/controller/UserControllerTest.java
@@ -0,0 +1,74 @@
+package com.stay.stay.controller;
+
+import com.stay.stay.constants.documentation.UserDocumentation;
+import com.stay.stay.domain.User;
+import com.stay.stay.dto.user.UserTosDto;
+import com.stay.stay.service.UserService;
+import org.apache.tomcat.jni.Local;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.time.LocalDate;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = UserController.class)
+@AutoConfigureRestDocs
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    public void 약관_동의() throws Exception {
+
+        //given
+        User user = User.builder()
+                .id(1L)
+                .name("민수")
+                .profileImage(null)
+                .tosAgreeDate(null)
+                .stampSet(null)
+                .placeSet(null)
+                .friendSet(null)
+                .isPrivate(true)
+                .isCompliedToday(true)
+                .currentRecord(0)
+                .bestRecord(0)
+                .build();
+
+        UserTosDto response = UserTosDto.builder()
+                .agreeDate(LocalDate.now())
+                .build();
+
+        given(userService.updateTosAgreeDate(any())).willReturn(response);
+
+        //when
+        ResultActions result = mockMvc.perform(put("/user/terms")
+                .header("userIndex", user.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        );
+
+        //then
+        result.andDo(print())
+                .andExpect(status().isOk())
+                .andDo(UserDocumentation.updateTos());
+    }
+}
