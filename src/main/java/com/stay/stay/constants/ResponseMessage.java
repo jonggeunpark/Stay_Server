23cd49a950f5c847d997648544c5f3772ca40dc9
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2021-01-17 17:41:29 +0900

    feat: #17 내 장소 변경 - 구현

--- src/main/java/com/stay/stay/constants/ResponseMessage.java
+++ src/main/java/com/stay/stay/constants/ResponseMessage.java
@@ -4,9 +4,11 @@ public class ResponseMessage {
 
     public static final String Update_Tos_Agree_Date = "약관 동의 날짜 변경 성공";
     public static final String Update_privacy = "기록 공개 여부 변경 성공";
+
     public static final String Create_place = "내 장소 추가 성공";
     public static final String Read_place_all = "내 장소 조회 성공";
     public static final String Read_place = "내 장소 상세조회 성공";
+    public static final String Update_place = "내 장소 변경 성공";
 
 
     public static final String NotFound = "Not Found Exception";
