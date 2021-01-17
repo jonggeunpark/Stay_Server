e91cd354d20d4f6187009baa493e7ccf85536b72
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2021-01-17 19:02:33 +0900

    feat: #21 공지사항 목록 조회 - 구현

--- src/main/java/com/stay/stay/controller/NoticeController.java
+++ src/main/java/com/stay/stay/controller/NoticeController.java
@@ -1,3 +1,38 @@
+package com.stay.stay.controller;
+
+import com.stay.stay.constants.Message;
+import com.stay.stay.constants.ResponseMessage;
+import com.stay.stay.constants.StatusCode;
+import com.stay.stay.domain.User;
+import com.stay.stay.dto.notice.NoticeDetailDto;
+import com.stay.stay.dto.notice.NoticeDto;
+import com.stay.stay.dto.place.PlaceDto;
+import com.stay.stay.service.NoticeService;
+import com.stay.stay.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/notice")
+public class NoticeController {
+
+    private final UserService userService;
+    private final NoticeService noticeService;
+
+    /** 공지사항 목록 조회 */
+    @GetMapping()
+    public ResponseEntity<Message> readNoticeAll(@RequestHeader("userIndex") Long userId) {
+        User user = userService.findById(userId);
+        List<NoticeDto> response = noticeService.readNoticeAll(userId);
+
+        Message message = new Message(StatusCode.OK, ResponseMessage.Read_notice_all, response);
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
 
     /** 내 장소 상세조회 */
     @GetMapping("/{noticeIndex}")
