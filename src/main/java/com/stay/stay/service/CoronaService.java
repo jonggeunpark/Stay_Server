ecb9127e4fc8b9aa25b370a3d006c5194af8f83e
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2021-01-25 19:59:08 +0900

    Feat: #33 코로나 데이터 업데이트 api 구현

--- src/main/java/com/stay/stay/service/CoronaService.java
+++ src/main/java/com/stay/stay/service/CoronaService.java
@@ -19,12 +19,20 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.*;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Objects;
 
 @Service
 @Transactional(readOnly = true)
@@ -40,42 +48,76 @@ public class CoronaService {
     @Transactional
     public void saveCorona(Corona corona) {coronaRepository.save(corona);}
 
+    private static String getTagValue(String tag, Element eElement) {
+        NodeList nlList = eElement.getElementsByTagName(tag).item(0).getChildNodes();
+        Node nValue = (Node) nlList.item(0);
+        if(nValue == null)
+            return null;
+        return nValue.getNodeValue();
+    }
+
+    public static Date addDay(Date date, int days) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.DAY_OF_MONTH, days);
+        return cal.getTime();
+    }
 
     @Transactional
-    //@Scheduled(cron = "0 30 * * * *")
+    @Scheduled(cron = "0 0 10 * * *")
     public void updateCorona() {
 
-        String pageNo = "1";
-        String numOfRows = "10";
+        Corona corona = findById(1L);
+        String ServiceKey = Covid19.ServiceKey;
         Date today = new Date();
+        Date yesterday = addDay(today, -1);
+
         SimpleDateFormat date = new SimpleDateFormat("yyyyMMdd");
-        String startCreateDt = date.format(today);
+        String startCreateDt = date.format(yesterday);
+        String endCreateDt = date.format(today);
 
         try{
 
-            StringBuilder urlBuilder = new StringBuilder("http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson"); /*URL*/
-            urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=ServiceKey"); /*Service Key*/
-            urlBuilder.append("&" + URLEncoder.encode("startCreateDt","UTF-8") + "=" + URLEncoder.encode("20200310", "UTF-8")); /*검색할 생성일 범위의 시작*/
-            urlBuilder.append("&" + URLEncoder.encode("endCreateDt","UTF-8") + "=" + URLEncoder.encode("20200315", "UTF-8")); /*검색할 생성일 범위의 종료*/
+            StringBuilder urlBuilder = new StringBuilder("http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson");
+            urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=" + ServiceKey);
+            urlBuilder.append("&" + URLEncoder.encode("startCreateDt","UTF-8") + "=" + URLEncoder.encode(startCreateDt, "UTF-8"));
+            urlBuilder.append("&" + URLEncoder.encode("endCreateDt","UTF-8") + "=" + URLEncoder.encode(endCreateDt, "UTF-8"));
             URL url = new URL(urlBuilder.toString());
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Content-type", "application/json");
-            System.out.println("Response code: " + conn.getResponseCode());
-            BufferedReader rd;
-            if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-                rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            } else {
-                rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+
+            DocumentBuilderFactory dbFactoty = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactoty.newDocumentBuilder();
+            Document doc = dBuilder.parse(String.valueOf(url));
+
+            doc.getDocumentElement().normalize();
+
+            NodeList nList = doc.getElementsByTagName("item");
+
+            int decideYesterday = -1;
+            int decideToday = -1;
+            int cnt;
+            String stateDt = startCreateDt;
+
+            for(int temp = 0; temp < nList.getLength(); temp++){
+                Node nNode = nList.item(temp);
+                if(nNode.getNodeType() == Node.ELEMENT_NODE){
+
+                    Element eElement = (Element) nNode;
+                    if(Objects.equals(getTagValue("stateDt", eElement), startCreateDt)) {
+                        decideYesterday = Integer.parseInt(Objects.requireNonNull(getTagValue("decideCnt", eElement)));
+                    }
+
+                    if(Objects.equals(getTagValue("stateDt", eElement), endCreateDt)) {
+                        decideToday = Integer.parseInt(Objects.requireNonNull(getTagValue("decideCnt", eElement)));
+                        stateDt = getTagValue("STATE_DT", eElement);
+                    }
+
+                }
             }
-            StringBuilder sb = new StringBuilder();
-            String line;
-            while ((line = rd.readLine()) != null) {
-                sb.append(line);
-            }
-            rd.close();
-            conn.disconnect();
-            System.out.println(sb.toString());
+
+            cnt = decideToday - decideYesterday;
+            corona.setSTATE_DT(stateDt);
+            corona.setDECIDE_CNT(cnt);
+            saveCorona(corona);
 
         } catch (Exception e) {
             e.printStackTrace();
