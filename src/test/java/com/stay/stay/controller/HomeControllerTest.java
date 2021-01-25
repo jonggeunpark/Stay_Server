d497214228c90beead0a2124c55da5f341a33b17
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2021-01-26 02:38:03 +0900

    fix: junit4 -> junit5

--- src/test/java/com/stay/stay/controller/HomeControllerTest.java
+++ src/test/java/com/stay/stay/controller/HomeControllerTest.java
@@ -6,7 +6,7 @@ import com.stay.stay.dto.corona.CoronaDto;
 import com.stay.stay.dto.home.HomeDto;
 import com.stay.stay.service.HomeService;
 import com.stay.stay.service.UserService;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
